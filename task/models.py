8922f8e11fb83598ba865e35b357fa642549b649
leonardo <leonardo.m.cruz@hotmail.com>
2020-08-28 22:29:38 -0300

    Created app task and improved model

---
+++ task/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Task(models.Model):
+    user = models.ForeignKey(
+        "user.User",
+        on_delete=models.CASCADE,
+        related_name="task"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+    final_date = models.DateTimeField(
+        null=True
+    )
+    content = models.TextField(
+        null=False,
+    )
+    concluded = models.BooleanField(
+        null=False,
+        default=False
+    )
