7c8b9ddadb116d26a1be0b5e6096e760b9815503
leonardo <leonardo.m.cruz@hotmail.com>
2020-09-02 12:08:10 -0300

    Created detail view of task app, renamed field of task model and added view to urls

--- task/models.py
+++ task/models.py
@@ -22,7 +22,7 @@ class Task(models.Model):
         null=False,
         default=False
     )
-    finished_date = models.DateTimeField(
+    concluded_date = models.DateTimeField(
         null=True
     )
     category = models.ForeignKey(
---
+++ task/views/detail.py
@@ -0,0 +1,26 @@
+from task.serializers import TaskSerializer
+from task.models import Task
+from task.permissions import ReadOnly, IsOwnerOrStaff
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+
+from datetime import datetime
+
+class TaskDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated,IsOwnerOrStaff|ReadOnly]
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        if(request.data.get("concluded") == "true"):
+            request.data["concluded_date"] = datetime.now()
+        else:
+            request.data["concluded_date"] = None
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
